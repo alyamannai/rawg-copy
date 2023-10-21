413d158c2e98a7ed04c6bb7f51e52416c8a29dc2
alyamannai <alyamannaimat@gmail.com>
2023-10-21 09:43:13 +0400

    created app and made a card component

---
+++ src/components/GameCard/GameCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Stack,
+  Heading,
+  Text,
+  Image,
+  Divider,
+  ButtonGroup,
+  Button,
+} from "@chakra-ui/react";
+
+interface Props {
+  image: string;
+  alt?: string;
+  description: string;
+  gameName: string;
+}
+
+const GameCard = ({ image, description, alt, gameName }: Props) => {
+  return (
+    <>
+      <Card maxW="sm">
+        <CardBody>
+          <Image src={image} alt={alt} borderRadius="lg" />
+          <Stack mt="6" spacing="3">
+            <Heading size="md">{gameName}</Heading>
+            <Text>{description}</Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+      </Card>
+    </>
+  );
+};
+
+export default GameCard;
